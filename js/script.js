cb811422b6feec8d5620e1e3ce7aef56bb210ea4
Jhon Freiman Arias <jhonarias1322@gmail.com>
2021-02-24 06:57:25 -0500

    agregando clases con jquery

--- js/script.js
+++ js/script.js
@@ -1,11 +1,60 @@
 // jQuery(document).ready(function() {
 
 // });
-// funciones para que se ejecute jQuery
+// funcion para que se ejecute jQuery
 
 $(function() {
     'use strict';
 
-    $('main article:first');
+    //agregando clases
+    $('div.logo img').addClass('activo');
+    $('#navegacion').show();
+    $('main article:first').addClass('activo');
+    $('main article:last').addClass('activo');
 
+    $('.navegacion ul li a').on('click', function(e) {
+        $('.navegacion ul li a').removeClass('activo');
+        e.preventDefault();
+        $(this).addClass('activo');
+    })
+
+
+    // $('#menu').on('click', function() {
+    //     $('#navegacion').show();
+    // })
+
+    /*
+        $('div.logo img').on('mouseenter', function() {
+            console.log('sobre el logo');
+        });
+        $('div.logo img').on('mouseleave', function() {
+            console.log('afuera del logo');
+        });
+    */
+    /*
+        $('div.logo img').on('click', function() {
+            $(this).remove();
+        });
+
+    */
+    /*
+        $('div.logo img').on('click', function() {
+            console.log('has hecho click en el logo');
+        });
+        */
+    /*
+        $('main article:first').hide();
+
+        var copia = $('main article:last').clone();
+        //copia clona el final
+        // $('main').append(copia);
+        //copia el comienzo
+        // $('main').prepend(copia);
+        //pega al comienzo
+        // $(copia).prependTo('main');
+        //copia el final
+        // $(copia).appendTo('main');
+
+
+    */
 });
